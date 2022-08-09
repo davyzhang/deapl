5959f993c1b164bdfdeea19bf86d975124bd3ea2
DavyZhang <davyzhang@gmail.com>
2022-08-09 20:17:02 +0930

    debug

--- src/translate.ts
+++ src/translate.ts
@@ -94,13 +94,15 @@ export async function translatePhrase(text: string, options: Options) {
   }
   await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36')
   await page.goto('https://www.deepl.com/translator')
-
+  setTimeout(() => {
+    page.screenshot({
+      type: 'jpeg',
+      path: '/tmp/s.jpg',
+    })
+  }, 1000 * 30)
   await page.waitForSelector('.lmt__language_select--target .lmt__language_select__active',
     queryWait)
-  await page.screenshot({
-    type: 'jpeg',
-    path: '/tmp/s.jpg',
-  })
+
   while (await hasSelector(page, '.dl_cookieBanner--buttonSelected')) {
     await page.click('.dl_cookieBanner--buttonSelected')
     await sleepMs(1000)
